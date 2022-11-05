d3a912f0d583babc4ba1921b631860c72e2fe57d
tex707 <papoulidis.kosta@gmail.com>
2022-11-05 14:35:44 +0100

    Proba

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import pandas as pd
+from pandas import read_html
+import html5lib
+
+Champion_Name = input("Írd be a hős nevét: ")
+Champion_Name = Champion_Name.lower
+url = "https://www.leagueofgraphs.com/rankings/summoners/" + Champion_Name
+
+
+print(url)
